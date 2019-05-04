e47d72b8bb81bbc2cdb3467fa592a1d33a0e20db
hoolindao <hoomiao@qq.com>
2019-05-04 16:49:40 +0800

    read tcol and code first pratice

---
+++ c/tcpl/pratice1-1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    printf("hello world");
+    return 0;
+}
+
+///gcc pratice1-1.c -o pratice1-1 &&./pratice1-1
+/* ---Terminal--- */
+/* hello world   */
+/* ------End----- */
